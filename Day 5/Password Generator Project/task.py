c68360783a7e5251a08a3f7264a2a155627a063d
OkayWheresAamir <hashmi.aamir@outlook.com>
2024-09-15 22:43:40 +0530

    D5-T4 : completed password generator ⭐️

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,4 @@
+import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +8,22 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = []
+for i in range(0,nr_letters):
+    if len(password) == 0:
+        password.append(random.choice(letters))
+    password.insert(random.randint(0,len(password)-1),random.choice(letters))
+for i in range(0,nr_symbols):
+    if len(password) == 0:
+        password.append(random.choice(symbols))
 
+    password.insert(random.randint(0, len(password) - 1), random.choice(symbols))
+for i in range(0,nr_numbers):
+    if len(password) == 0:
+        password.append(random.choice(numbers))
+
+    password.insert(random.randint(0, len(password) - 1), random.choice(numbers))
+
+print("Your password is ",end='')
+for i in range(0,len(password)):
+    print(password[i],end='')
