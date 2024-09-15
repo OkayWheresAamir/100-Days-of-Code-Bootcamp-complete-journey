a3c3c484b2cd6957c71be11a33a8f691425321be
OkayWheresAamir <hashmi.aamir@outlook.com>
2024-09-15 18:38:16 +0530

    updated solution

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,5 +1,4 @@
-print("Welcome to the Band Name Generator.")
-city = input("What's the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-band_name = city + " " + pet
-print(f"Your band name could be {band_name}")
+# Write your code below this line 👇
+print("Hello"+ " " + "Angela")
+
+
