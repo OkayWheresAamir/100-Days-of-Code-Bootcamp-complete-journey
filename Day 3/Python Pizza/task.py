36a02c662c5935f051a14d8240052eee7b6c5629
OkayWheresAamir <hashmi.aamir@outlook.com>
2024-09-15 19:29:42 +0530

    D3-T5 : completed python pizza

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,23 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+if size.upper() == "S":
+    cost = 15
+    if pepperoni.upper() == "Y":
+        cost += 2
+
+elif size.upper() == "M":
+    cost = 20
+    if pepperoni.upper() == "Y":
+        cost += 3
+elif size.upper() == "L":
+    cost = 25
+    if pepperoni.upper() == "Y":
+        cost += 3
+
+
+
+if extra_cheese.upper() == "Y":
+    cost +=1
+
+print(f"Your final bill is: ${cost}")
