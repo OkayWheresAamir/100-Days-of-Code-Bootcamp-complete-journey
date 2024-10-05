9e351cf04660f81710aa889d15dac0b1b65ada98
OkayWheresAamir <hashmi.aamir@outlook.com>
2024-10-06 03:16:08 +0530

    D8-T3 : caesar cipher done 1/3

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -4,8 +4,16 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    encrypted = ""
+    for i in original_text:
+
+        new_index = alphabet.index(i) + shift_amount
+        if new_index > 25:
+            new_index -= 26
+        encrypted += alphabet[new_index]
+    print(encrypted)
 
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
@@ -15,3 +23,4 @@ shift = int(input("Type the shift number:\n"))
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
 
+encrypt(text,shift)
