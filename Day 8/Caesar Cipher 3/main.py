3ff7adb7505462801213e2775d6f8a515795598c
OkayWheresAamir <hashmi.aamir@outlook.com>
2024-10-06 16:00:34 +0530

    D8-T3 : caesar cipher done 3/3

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,5 +1,8 @@
 # TODO-1: Import and print the logo from art.py when the program starts.
 
+from art import logo
+print(logo)
+
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -12,7 +15,9 @@ def caesar(original_text, shift_amount, encode_or_decode):
     for letter in original_text:
         if encode_or_decode == "decode":
             shift_amount *= -1
-
+        if letter not in alphabet:
+            output_text += letter
+            continue
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         output_text += alphabet[shifted_position]
@@ -21,12 +26,13 @@ def caesar(original_text, shift_amount, encode_or_decode):
 
 # TODO-3: Can you figure out a way to restart the cipher program?
 
+choice = input("Do you want to encrypt/decrypt? (y/n) : ")
+while choice.lower() == "y":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    choice = input("Do you want to try again? (y/n) : ")
 
 
