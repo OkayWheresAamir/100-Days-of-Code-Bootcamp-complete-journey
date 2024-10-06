7b4e56bc7a7f66e53be2816356b95827e695c18a
OkayWheresAamir <hashmi.aamir@outlook.com>
2024-10-06 18:59:29 +0530

    D8-T3 : blind auction done

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -4,3 +4,19 @@
 # TODO-4: Compare bids in dictionary
 
 
+print("Welcome to the auction!")
+other = 'y'
+
+bidders = {}
+max_bid = 0
+while other.lower() == 'y':
+    name = input("Enter your name : ")
+    bid = int(input("What's your bid? : $"))
+    other = input("Are there any other bidders? (y/n)")
+    bidders[name] = bid
+    if max_bid < bid:
+        max_bid = bid
+    print("\n" * 20)
+
+
+print(f"And the max bid is ${max_bid}")
